008017be2a2c00527b186899f219f0a2c0439bdc
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-02-06 11:44:08 +0200

    Adding Inline comments & Refactoring the code for Registeration Form.

--- Registeration Form with Combine and View Model/ContentView.swift
+++ Registeration Form with Combine and View Model/ContentView.swift
@@ -13,48 +13,21 @@ struct ContentView: View {
     @State private var passwordConfirm = ""
     var body: some View {
         VStack {
-            Text("Create an Account")
-                .font(.system(.largeTitle, design: .rounded))
-                .bold()
-                .padding(.bottom,30)
-            FormField(fieldName: "UserName", fieldValue: $username)
-            RequirmentText(text:"A minimum of 4 characters")
-                .padding()
-            FormField(fieldName: "Password", fieldValue: $password, isSecure: true)
-            VStack {
-                RequirmentText(iconName: "lock.open", text: "A minimum of 8 characters", isStrikeThrough: false)
-                RequirmentText(iconName: "lock.open", text: "One Uppercase Letter", isStrikeThrough: false)
-            }
-            .padding()
-            FormField(fieldName: "Confirm Password", fieldValue: $passwordConfirm, isSecure: true)
-            RequirmentText(text:"Your confirm password should be the same as password",isStrikeThrough: false)
-                .padding()
-                .padding(.bottom,50)
+            HeaderView()
+            UserNameView(username: $username)
+            PasswordView(password: $password, passwordConfirm: $passwordConfirm)
             Button(action: {
                 // To Next Screen
-            }, label: {
-                Text("Sign Up")
-                    .font(.system(.body, design: .rounded))
-                    .foregroundColor(.white)
-                    .bold()
-                    .padding()
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                    .background(LinearGradient(gradient: Gradient(colors: [Color(red: 251/255, green: 128/255, blue: 128/255), Color(red: 253/255, green: 193/255, blue: 104/255)]), startPoint: .leading, endPoint: .trailing))
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-            })
+            }) {
+            }.buttonStyle(LongButtonStyle())
             HStack{
                 Text("Already have an Account?")
                     .font(.system(.body, design: .rounded))
                     .bold()
                 Button(action: {
                     //To Sign IN Screen
-                }, label: {
-                    Text("Sign in")
-                        .font(.system(.body, design: .rounded))
-                        .bold()
-                        .foregroundColor(Color(red: 251/255, green: 128/255, blue: 128/255))
-                })
+                }){
+                }.buttonStyle(TextButtonStyle())
             }
             .padding(.top,50)
             Spacer()
@@ -81,7 +54,7 @@ struct RequirmentText : View {
             Text(text)
                 .font(.system(.body, design: .rounded))
                 .foregroundColor(.secondary)
-                .strikethrough(isStrikeThrough)
+                .strikethrough(isStrikeThrough) // Applies strike through on text based on Boolean Argument.Default value for strike through is true.
             Spacer()
         }
     }
@@ -94,7 +67,7 @@ struct FormField : View {
     var body: some View {
         VStack {
             if isSecure {
-                SecureField(fieldName, text: $fieldValue)
+                SecureField(fieldName, text: $fieldValue) // Secure Field Automatically masks the user's input.
                     .font(.system(size: 20, weight: .semibold, design: .rounded))
                     .padding(.horizontal)
             }
@@ -103,10 +76,63 @@ struct FormField : View {
                     .font(.system(size: 20, weight: .semibold, design: .rounded))
                     .padding(.horizontal)
             }
-            Divider()
+            Divider() // Line After Text Field.
                 .frame(height: 1)
                 .background(Color(red: 240/255, green: 240/255, blue: 240/255))
                 .padding(.horizontal)
         }
+    }
+}
+struct HeaderView : View {
+    var body: some View {
+        Text("Create an Account")
+            .font(.system(.largeTitle, design: .rounded))
+            .bold()
+            .padding(.bottom,30)
+    }
+}
+struct UserNameView : View {
+    @Binding var username : String
+    var body: some View {
+        FormField(fieldName: "UserName", fieldValue: $username)
+        RequirmentText(text:"A minimum of 4 characters")
+            .padding()
+    }
+}
+struct PasswordView : View {
+    @Binding var password : String
+    @Binding var passwordConfirm : String
+    var body: some View {
+        FormField(fieldName: "Password", fieldValue: $password, isSecure: true)
+        VStack(spacing: 5) {
+            RequirmentText(iconName: "lock.open", text: "A minimum of 8 characters", isStrikeThrough: false)
+            RequirmentText(iconName: "lock.open", text: "One Uppercase Letter", isStrikeThrough: false)
+        }
+        .padding()
+        FormField(fieldName: "Confirm Password", fieldValue: $passwordConfirm, isSecure: true)
+        RequirmentText(text:"Your confirm password should be the same as password",isStrikeThrough: false)
+            .padding()
+            .padding(.bottom,50)
+    }
+}
+struct LongButtonStyle : ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Text("Sign Up")
+            .font(.system(.body, design: .rounded))
+            .foregroundColor(.white)
+            .bold()
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(red: 251/255, green: 128/255, blue: 128/255), Color(red: 253/255, green: 193/255, blue: 104/255)]), startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(10)
+            .padding(.horizontal)
+    }
+}
+struct TextButtonStyle : ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Text("Sign in")
+            .font(.system(.body, design: .rounded))
+            .bold()
+            .foregroundColor(Color(red: 251/255, green: 128/255, blue: 128/255))
     }
 }
